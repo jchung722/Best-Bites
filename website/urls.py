a098d2186eac3937301e70387489dc1be0a885e8
jchung722 <jchung722@gmail.com>
2017-11-17 14:24:27 -0800

    added user view

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^dish/(?P<pk>\d+)/$', views.dish_detail, name='dish_detail'),
+    url(r'^user/(?P<pk>\d+)/$', views.user_detail, name='user_detail'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Dish, Restaurant, Review
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
@@ -11,3 +12,8 @@ def dish_detail(request, pk):
     reviews = Review.objects.filter(dish=dish)
     dishes = Dish.objects.filter(restaurant = dish.restaurant)
     return render(request, 'website/dish_detail.html', {'dish': dish, 'reviews':reviews, 'dishes':dishes})
+
+def user_detail(request, pk):
+    user = User.objects.get(pk=pk)
+    reviews = Review.objects.filter(author=user)
+    return render(request, 'website/user_detail.html', {'user': user, 'reviews':reviews,})
