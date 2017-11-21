eb2382a5f0dad9f384f24f19acafff31984f858b
jchung722 <jchung722@gmail.com>
2017-11-21 06:45:13 -0800

    update restaurant page

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^dish/(?P<pk>\d+)/$', views.dish_detail, name='dish_detail'),
+    url(r'^restaurant/(?P<pk>\d+)/$', views.restaurant_detail, name='restaurant_detail'),
     url(r'^user/(?P<pk>\d+)/$', views.user_detail, name='user_detail'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^login/$', auth_views.login, {'template_name': 'website/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
 from .models import Dish, Restaurant, Review
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .forms import SignUpForm, ReviewForm
 
 # Create your views here.
 def home(request):
@@ -11,9 +15,39 @@ def dish_detail(request, pk):
     dish = Dish.objects.get(pk=pk)
     reviews = Review.objects.filter(dish=dish)
     dishes = Dish.objects.filter(restaurant = dish.restaurant)
-    return render(request, 'website/dish_detail.html', {'dish': dish, 'reviews':reviews, 'dishes':dishes})
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.author = request.user
+            review.dish = dish
+            review.created_date = timezone.now()
+            review.save()
+            return redirect('dish_detail', pk=dish.pk)
+    else:
+        form = ReviewForm()
+        return render(request, 'website/dish_detail.html', {'form':form, 'dish':dish, 'reviews':reviews, 'dishes':dishes})
+
+def restaurant_detail(request, pk):
+    restaurant = Restaurant.objects.get(pk=pk)
+    dishes = Dish.objects.filter(restaurant = restaurant)
+    return render(request, 'website/restaurant_detail.html', {'restaurant':restaurant, 'dishes':dishes,})
 
 def user_detail(request, pk):
     user = User.objects.get(pk=pk)
     reviews = Review.objects.filter(author=user)
     return render(request, 'website/user_detail.html', {'user': user, 'reviews':reviews,})
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'website/signup.html', {'form': form})
